393d4e73ef51dbb8c5e1ef5c9ecb0488dba3d82f
saubor <163947257+saubor@users.noreply.github.com>
2024-05-17 17:03:06 -0300

    Add files via upload

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+resultado="o resultado é"
+v1=10
+v2=3.5
+v3=v1+v2
+print(resultado)
+print(v1)
+print(v2)
+print(v3)
+print(type(resultado))
+print(type(v1))
+print(type(v2))
+print(type(v3))
---
+++ modulo2/aula2_questao4.py
@@ -0,0 +1,7 @@
+saldo = 500.0
+juros = 1.01
+saldo = saldo*juros
+saldo = saldo*juros
+saldo = saldo*juros
+print("Após 3 meses meu saldo é:")
+print(saldo)
